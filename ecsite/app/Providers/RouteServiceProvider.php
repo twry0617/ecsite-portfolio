dd139fb395c44d9e60f8b32b4b0b53bceb72f851
takumi_kurogi <takumines1@gmail.com>
2020-05-27 00:10:18 +0900

    [update]新規登録後の遷移先修正

--- ecsite/app/Providers/RouteServiceProvider.php
+++ ecsite/app/Providers/RouteServiceProvider.php
@@ -16,13 +16,6 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected $namespace = 'App\Http\Controllers';
 
-    /**
-     * The path to the "home" route for your application.
-     *
-     * @var string
-     */
-    public const HOME = '/home';
-
     // Supplierのリダイレクト先
     public const SUPPLIER_HOME = '/supplier/home';
 
