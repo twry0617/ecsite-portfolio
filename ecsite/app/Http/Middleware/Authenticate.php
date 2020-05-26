90e29522052ef7444b90e0ac00b6b7de628ea4b8
takumi_kurogi <takumines1@gmail.com>
2020-05-26 21:29:04 +0900

    [update]未ログイン時の挙動修正
    Consumer,Managerの追加による修正を行った

--- ecsite/app/Http/Middleware/Authenticate.php
+++ ecsite/app/Http/Middleware/Authenticate.php
@@ -7,12 +7,28 @@ use Illuminate\Auth\Middleware\Authenticate as Middleware;
 
 class Authenticate extends Middleware
 {
+
+    /**
+     * consumerのログインURL
+     *
+     * @var string
+     */
+    protected $consumer_route = 'consumer.login';
+
     /**
      * supplierのログインURL
      *
      * @var string
      */
     protected $supplier_route = 'supplier.login';
+
+    /**
+     * managerのログインURL
+     *
+     * @var string
+     */
+    protected $manager_route = 'manager.login';
+
     /**
      * Get the path the user should be redirected to when they are not authenticated.
      *
@@ -22,8 +38,12 @@ class Authenticate extends Middleware
     protected function redirectTo($request)
     {
         if (! $request->expectsJson()) {
-            if (Route::is('supplier.*')) {
+            if (Route::is('consumer.*')) {
+                return route($this->consumer_route);
+            } elseif (Route::is('supplier.*')) {
                 return route($this->supplier_route);
+            } elseif (Route::is('manager.*')) {
+                return route($this->manager_route);
             }
         }
     }
