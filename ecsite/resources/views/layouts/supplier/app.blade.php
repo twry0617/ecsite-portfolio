8f1d4a2d40a981fb6bd1b295869600729d3c8f95
takumi_kurogi <takumines1@gmail.com>
2020-05-28 15:59:16 +0900

    [update]supplierの新規登録は招待制なのでリンクを削除

--- ecsite/resources/views/layouts/supplier/app.blade.php
+++ ecsite/resources/views/layouts/supplier/app.blade.php
@@ -44,11 +44,6 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('supplier.login') }}">{{ __('Login') }}</a>
                             </li>
-                            @if (Route::has('supplier.register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('supplier.register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
