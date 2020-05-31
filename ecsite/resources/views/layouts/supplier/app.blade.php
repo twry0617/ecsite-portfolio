3c7b41021926feb39928450a578f5b9bd56d09dd
takumi_kurogi <takumines1@gmail.com>
2020-05-31 18:53:47 +0900

    [update]supplierのレイアウトviewの修正

--- ecsite/resources/views/layouts/supplier/app.blade.php
+++ ecsite/resources/views/layouts/supplier/app.blade.php
@@ -44,6 +44,10 @@
                             <li class="nav-item">
                                 <a class="nav-link" href="{{ route('supplier.login') }}">{{ __('Login') }}</a>
                             </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('supplier.invitation') }}">{{ __('仮登録申請') }}</a>
+                            </li>
                         @else
                             <li class="nav-item dropdown">
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
