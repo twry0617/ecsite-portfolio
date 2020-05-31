079c62f22997980cfe5a12da66d721ab3cfe1803
takumi_kurogi <takumines1@gmail.com>
2020-05-31 18:55:02 +0900

    [add]Supplierの仮登録申請画面の追加

---
+++ ecsite/resources/views/manager/auth/invitation.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.manager.manager')
+
+@section('content')
+        @if (session('flash_message'))
+            <div class="flash_message">
+                {{ session('flash_message') }}
+            </div>
+        @endif
+        <div class="justify-content-center flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
+        <div class="card mt-5">
+                <div class="card-header">{{ __('supplier招待') }}</div>
+
+                <div class="card-body">
+                    <form method="POST" action="{{ route('manager.invitation') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email">
+                            </div>
+                        </div>
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-6 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('招待する') }}
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+@endsection
---
+++ ecsite/resources/views/supplier/auth/invitation.blade.php
@@ -0,0 +1,40 @@
+@extends('layouts.supplier.app')
+
+@section('content')
+        @if (session('flash_message'))
+            <div class="flash_message">
+                {{ session('flash_message') }}
+            </div>
+        @endif
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-header">{{ __('仮登録申請') }}</div>
+
+                        <div class="card-body">
+                            <form method="POST" action="{{ route('supplier.invitation') }}">
+                                @csrf
+
+                                <div class="form-group row">
+                                    <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+
+                                    <div class="col-md-6">
+                                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email">
+                                    </div>
+                                </div>
+
+                                <div class="form-group row mb-0">
+                                    <div class="col-md-6 offset-md-4">
+                                        <button type="submit" class="btn btn-primary">
+                                            {{ __('仮登録を申し込む') }}
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
