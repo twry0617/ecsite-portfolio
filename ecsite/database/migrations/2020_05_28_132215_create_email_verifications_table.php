ba1ce74a004a0a415bdeb23586d12643acdc05a8
takumi_kurogi <takumines1@gmail.com>
2020-05-31 18:47:45 +0900

    [add]仮登録用のテーブルを追加

---
+++ ecsite/database/migrations/2020_05_28_132215_create_email_verifications_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEmailVerificationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('email_verifications', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('email', 255)->comment('メールアドレス');
+            $table->string('token', 250)->comment('確認トークン');
+            $table->tinyInteger('status')->comment('ステータス');
+            $table->dateTime('expiration_datetime')->comment('有効期限');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('email_verifications');
+    }
+}
