1d4aa9362407db1378f5e929d446a8195a30c5d6
takumi_kurogi <takumines1@gmail.com>
2020-06-11 21:48:10 +0900

    [add]productテーブルを作成

---
+++ ecsite/database/migrations/2020_06_09_114106_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('supplier_id')->unsigned();
+            $table->string('name', 100);
+            $table->string('code', 45);
+            $table->string('description', 500);
+            $table->integer('price');
+            $table->integer('stock');
+            $table->string('status', 45);
+            $table->timestamps();
+
+            $table->foreign('supplier_id')->references('id')->on('suppliers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
