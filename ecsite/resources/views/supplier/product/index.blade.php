f9a9e3db3238450ce4007fb5c649974ba5d15161
takumi_kurogi <takumines1@gmail.com>
2020-06-15 23:16:01 +0900

    [add]商品一覧用のbladeファイル作成

---
+++ ecsite/resources/views/supplier/product/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.supplier.app')
+
+@section('content')
+    @if(Session::has('flash_message'))
+        <div class="alert alert-success">
+            {{ session('flash_message') }}
+        </div>
+    @endif
+    <div class="container">
+        <div class="row justify-content-left">
+            @foreach ($productList as $product)
+            <div class="col-md-4 mb-2">
+                <div class="card">
+                    <div class="card-header">
+                        <a href="/product/{{ $product }}">{{ $product->name }}</a>
+                    </div>
+                    <div class="card-body">
+                        {{ $product->price }}円
+                    </div>
+                    @auth
+                        <form method="POST" action="cartitem" class="form-inline m-1">
+                            {{ csrf_field() }}
+                            <select name="quantity" class="form-control col-md-2 mr-1">
+                                <option selected>1</option>
+                                <option>2</option>
+                                <option>3</option>
+                                <option>4</option>
+                                <option>5</option>
+                            </select>
+                            <input type="hidden" name="item_id" value="{{ $product->id }}">
+                            <button type="submit" class="btn btn-primary col-md-6">カートに入れる</button>
+                        </form>
+                    @endauth
+                </div>
+            </div>
+            @endforeach
+        </div>
+    </div>
+@endsection
