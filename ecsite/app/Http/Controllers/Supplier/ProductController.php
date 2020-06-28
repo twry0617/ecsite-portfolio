2796c5497a92e2292b76a87c9d7075c77e3d4c72
takumi_kurogi <takumines1@gmail.com>
2020-06-28 23:48:51 +0900

    [add]ProductControllerの修正

--- ecsite/app/Http/Controllers/Supplier/ProductController.php
+++ ecsite/app/Http/Controllers/Supplier/ProductController.php
@@ -7,6 +7,11 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Photo;
 use App\Models\Option;
+use App\Http\Requests\ProductStore;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Intervention\Image\Facades\Image;
 
 class ProductController extends Controller
 {
@@ -18,21 +23,17 @@ class ProductController extends Controller
      */
     public function index(Product $product, Photo $photo)
     {
-        $products = $product->all();
-        foreach ($products as $product) {
-            $product->photos;
-            $productList[] = $product;
-        }
+        $products = $product->paginate(6);
 
         return view('supplier.product.index',[
-            'productList' => $productList
+            'products' => $products
         ]);
     }
 
     /**
      * Show the form for creating a new resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
      */
     public function create()
     {
@@ -42,35 +43,65 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param ProductStore $request
+     * @param Product $product
+     * @param Photo $photo
+     * @param Option $option
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request, Product $product, Photo $photo, Option $option)
+    public function store(ProductStore $request, Product $product, Option $option)
     {
-        $supplier = $request->user();
+        $supplier = DB::transaction(function () use ($request, $product, $option) {
+            $supplier = $request->user();
+            $product->supplier_id = $supplier->id;
+            $product->fill($request->all())->save();
 
-        $product->supplier_id = $supplier->id;
-        $product->fill($request->all())->save();
+            $option->product_id = $product->id;
+            $option->fill($request->all())->save();
 
-        $photo->product_id = $product->id;
-        $photo->fill($request->all())->save();
+            if (empty($request->file('photo'))) {
+                return redirect()->route('supplier.product.list');
+            }
 
-        $option->product_id = $product->id;
-        $option->fill($request->all())->save();
+            foreach ($request->file('photo') as $image) {
+                $photo = new Photo;
+                $photo->product_id = $product->id;
+                Log::info($image);
+                $extension = $image->getClientOriginalExtension();
+                // 画像の名前を取得
+                $filename = $image->getClientOriginalName();
+                // 画像をリサイズ
+                $resize_img = Image::make($image)->resize(320, 240)->encode($extension);
+                // 画像のpath
+                $path = 'product/' . $request->name . '/' . $filename;
+                // s3のuploadsファイルに追加
+                Storage::disk('local')->put($path, (string)$resize_img, 'public');
+                // 画像のURLを参照
+                $url = Storage::disk('local')->url($path);
+                $photo->photo = $url;
+                $photo->save();
+            }
+
+            return $supplier;
+        });
+
 
         return redirect()->route('supplier.product.list');
-
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * @param  Product $product
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Http\Response|\Illuminate\View\View
      */
     public function show(Product $product)
     {
-        //
+        $photos = $product->photos;
+        return view('supplier.product.show', [
+            'product' => $product,
+            'photos' => $photos,
+        ]);
     }
 
     /**
